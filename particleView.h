5a97f58c27c358ab9fa5be6598c52c58edd6f602
set830 <shawnthomas@chicagobooth.edu>
2014-12-15 17:07:49 -0600

    blew it up, then started after a check out

---
+++ particleView.h
@@ -0,0 +1,18 @@
+//
+//  particleView.h
+//  ParticleEffect
+//
+//  Created by Shawn Thomas on 4/9/13.
+//  Copyright (c) 2013 Midikinz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface particleView : UIView
+//testgitagain
+//testgitagainx2
+
+-(void)setEmitterPositionFromTouch: (UITouch*)t;
+-(void)setIsEmitting:(BOOL)isEmitting;
+
+@end
